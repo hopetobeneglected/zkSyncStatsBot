5105008e5899dc61847f06c118a3906167924eea
Vladislav Vtorushin / Computer <ge47gec@mytum.de>
2023-08-01 18:28:56 +0200

    Checker done req updated

--- checker.py
+++ checker.py
@@ -8,8 +8,9 @@ import json
 import random
 import requests
 
-chrome_options = webdriver.ChromeOptions()
+browser = None
 
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-infobars')
 chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
@@ -19,15 +20,34 @@ chrome_options.add_argument('log-level=3')
 chrome_options.add_argument("--headless")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
+headers = {
+                'authority': 'zksync2-mainnet.zkscan.io',
+                'accept': 'application/json, text/plain, */*',
+                'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                'origin': 'https://byfishh.github.io',
+                'referer': 'https://byfishh.github.io/',
+                'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/115.0.0.0 Safari/537.36',
+            }
 
-def start(wallet, max_retries=3):
+
+def get_stats(wallet, max_retries=3):
+
+    global browser
+
     logger.info(f"Statistic loading for {wallet}...")
     url_stats = f"https://byfishh.github.io/zk-flow/?address={wallet}"
-    browser = webdriver.Chrome(options=chrome_options)
 
     retries_left = max_retries
     for retry in range(max_retries):
         try:
+            browser = webdriver.Chrome(options=chrome_options)
             browser.get(url_stats)
             delay = random.randint(2, 5)
             sleep(delay)
@@ -44,15 +64,16 @@ def start(wallet, max_retries=3):
 
             logger.success(f"{wallet} has {amount_trx} transactions | {volume} volume | {fee_spent} fee spent")
             browser.quit()
-            break
+            return f"{amount_trx} transactions | {volume} volume | {fee_spent} fee spent"
 
         except (selenium.common.exceptions.WebDriverException, selenium.common.NoSuchElementException) as e:
-            logger.warning(f"Something went wrong, retrying wallet {wallet}... (Retries left: {retries_left})")
+            logger.warning(f"Something went wrong, retrying wallet {wallet}... (Retries left: {retries_left}), {e}")
             retries_left -= 1
 
     else:
         logger.error(f"Failed to retrieve stats for wallet {wallet}. Max retries exceeded.")
         browser.quit()
+        return "Failed to load the statistic. Please try again later"
 
 
 def get_wallets():
@@ -66,11 +87,23 @@ def get_balance(wallet, max_retries=3):
     retries_left = max_retries
     for retry in range(max_retries):
         try:
-            url_balances = f"https://zksync2-mainnet.zkscan.io/api?module=account&action=tokenlist&address={wallet}"
-            delay = random.randint(2, 5)
+            delay = random.randint(2, 6)
             sleep(delay)
 
-            response = requests.get(url_balances)
+            params = {
+                'module': 'account',
+                'action': 'tokenlist',
+                'address': wallet,
+            }
+
+            proxies = {
+                "http": "http://hdhxulgp-rotate:f7j8yo4tl2ev@p.webshare.io:80/",
+                "https": "http://hdhxulgp-rotate:f7j8yo4tl2ev@p.webshare.io:80/"
+            }
+
+            response = requests.get('https://zksync2-mainnet.zkscan.io/api', params=params, headers=headers,
+                                    proxies=proxies)
+
             response.raise_for_status()
 
             tokens_array = json.loads(response.text)["result"][:3]
@@ -78,22 +111,38 @@ def get_balance(wallet, max_retries=3):
                                   in tokens_array]
 
             logger.success(f"Balance of {wallet} : " + " | ".join(token_info_strings))
-            break
+            return f" | ".join(token_info_strings)
 
-        except requests.exceptions.RequestException as e:
+        except (requests.exceptions.RequestException, TypeError) as e:
             logger.warning(f"Something went wrong, retrying wallet {wallet}... (Retries left: {retries_left}), {e}")
             retries_left -= 1
 
     else:
-        logger.error(f"Failed to retrieve stats for wallet {wallet}. Max retries exceeded.")
+        logger.error(f"Failed to retrieve balances for wallet {wallet}. Max retries exceeded.")
+        return "Failed to load the balances. Please try again later"
 
 
 def calculate_amount(balance, decimals):
     return int(balance) / (10 ** int(decimals)) if decimals else int(balance)
 
 
-if __name__ == '__main__':
-    # pool = Pool(2)
-    # pool.map(get_balance, list(get_wallets()))
+def get_info(wallet):
+    if len(wallet) != 42 or wallet[:2] != "0x":
+        print(f"{wallet} seems to be invalid! Please enter the correct wallet!")
+        return
 
-    get_balance("0xF79998AD9B7b61294a1726f11f4897cFD9Ed20E7")
+    statistic = get_stats(wallet)
+    balances = get_balance(wallet)
+    print(f"\nWallet {wallet}\n\n"
+          f"Account stats: {statistic}\n"
+          f"Account balances: {balances}\n")
+
+
+def check_all():
+    pool = Pool(5)
+    pool.map(get_info, list(get_wallets()))
+
+
+if __name__ == '__main__':
+    get_info("0xF79998AD9B7b61294a1726f11f4897cFD9Ed20E7")
+
